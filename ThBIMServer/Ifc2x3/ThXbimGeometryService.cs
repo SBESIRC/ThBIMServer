a90be41a662dde3f818cc2541d1ad4b2366f6f13
董士崇 <dongshichong@thape.com.cn>
2022-10-14 21:16:29 +0800

    Use log4net

--- ThBIMServer/Ifc2x3/ThXbimGeometryService.cs
+++ ThBIMServer/Ifc2x3/ThXbimGeometryService.cs
@@ -5,10 +5,13 @@ namespace ThBIMServer.Ifc2x3
 {
     public sealed class ThXbimGeometryService
     {
+        //==============SINGLETON============
+        //fourth version from:
+        //http://csharpindepth.com/Articles/General/Singleton.aspx
         private static readonly Lazy<ThXbimGeometryService> lazy =
             new Lazy<ThXbimGeometryService>(() => new ThXbimGeometryService());
-
         public static ThXbimGeometryService Instance { get { return lazy.Value; } }
+        //-------------SINGLETON-----------------
 
         public readonly XbimGeometryEngine Engine;
 
